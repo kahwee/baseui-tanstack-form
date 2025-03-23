a520dc39a0c5d32952afbd19637ac4173d6355d9
KahWee Teng <262105+kahwee@users.noreply.github.com>
2025-03-22 22:44:25 -0700

    Update ESLint configuration and refactor schema formatting for consistency

--- src/features/people/people-schema.ts
+++ src/features/people/people-schema.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod'
 
 export const PeopleSchema = z.object({
-    firstName: z
-        .string()
-        .min(3, '[Zod] You must have a length of at least 3')
-        .startsWith('A', "[Zod] First name must start with 'A'"),
-    lastName: z.string().min(3, '[Zod] You must have a length of at least 3'),
+  firstName: z
+    .string()
+    .min(3, '[Zod] You must have a length of at least 3')
+    .startsWith('A', "[Zod] First name must start with 'A'"),
+  lastName: z.string().min(3, '[Zod] You must have a length of at least 3'),
 })
